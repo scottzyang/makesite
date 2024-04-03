fcf2a55cf865b3d52726a74a95f99ee0e1e7fbf9
scottzyang <scottzyang@gmail.com>
2024-04-02 21:48:41 -0500

    Complete reading and writing file to template and html

--- makesite.go
+++ makesite.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	// create new blank HTML file
+	file, err := os.Create("first-post.html")
+	if err != nil {
+		fmt.Println("Error creating HTML file", err)
+	}
+	// Open the file
+	contents, err := os.ReadFile("./first-post.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	// Parse the template
+	tmpl, err := template.ParseFiles("template.tmpl")
+	if err != nil {
+		fmt.Println("Error parsing template:", err)
+		return
+	}
+
+	// define data structure to pass to the template
+	// capitalized to be accessed in tmpl
+	type Content struct {
+		Content string
+	}
+
+	// create an instance of Content
+	contentData := Content{
+		Content: string(contents),
+	}
+
+	// execute the template with the content data and output to the HTML file.
+	if err := tmpl.Execute(file, contentData); err != nil {
+		fmt.Println("Error executing template:", err)
+		return
+	}
 }
